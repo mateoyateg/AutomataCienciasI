a477fcfb4bee1082943ab43d7eb15b2d38be3cee
Mateo Yate Gonzalez <30778491+mateoyateg@users.noreply.github.com>
2019-06-04 22:18:28 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,86 @@
+from Algoritmo.BoyerMoore import BoyerMoore
+import math
+
+def verificaPalindromo(palabra):
+    medio = int(math.ceil(len(palabra) / 2))
+    varPal = True
+
+    if len(palabra) > 0:
+
+        inf = 0
+        sup = len(palabra) - 1
+
+        for i in range(0, medio):
+
+            if palabra[inf] == palabra[sup]:
+                varPal = True
+
+            elif palabra[inf] != palabra[sup]:
+                varPal = False
+                return varPal
+
+            inf += 1
+            sup -= 1
+    return varPal
+
+def voltearCompleto(texto):
+    invertido = texto[::-1]
+    print("El texto completo invertido es: "+ invertido)
+
+def voltearPalabras(texto):
+    texto = texto.split(" ")
+    for i in range (len(texto)):
+        texto[i] = texto[i][::-1]
+    final = ''
+    for i in range (len(texto)):
+        final += texto[i]
+        final += " "
+    print("El texto invirtiendo las palabras es: "+ final)
+
+def mostrarPalabra(palabraInicial, palabraBuscada, palabraPalindroma):
+    palabraPartida = []
+
+    negrita = "\033[1;30m"
+    sinNegrita = "\033[0;37m"
+
+    negritaSub = "\033[4;30m"
+    sinNegritaSub = "\033[0;37m"
+
+    palabraPartida = palabraInicial.split(palabraBuscada)
+
+    if palabraPalindroma:
+        for i in range(len(palabraPartida)):
+            print(sinNegritaSub + palabraPartida[i], end="")
+            if i == len(palabraPartida) - 1:
+                pass
+            else:
+                print(negritaSub + palabraBuscada, end="")
+
+    else:
+        for i in range(len(palabraPartida)):
+            print(sinNegrita + palabraPartida[i], end="")
+            if i == len(palabraPartida) - 1:
+                pass
+            else:
+                print(negrita + palabraBuscada, end="")
+
+def main():
+    texto = "facasabfaabbdcasadkah ihaiodhamaraabdaab"
+    patron = "mara"
+    BoyerMoore(texto, patron).encontrarPatron()
+    voltearCompleto(texto)
+    voltearPalabras(texto)
+
+    palabraPalindroma = verificaPalindromo(patron)
+    print("¿La palabra es palindroma? " + str(palabraPalindroma))
+
+    coincidencia = True
+
+    if coincidencia:
+        mostrarPalabra(texto,patron, palabraPalindroma)
+    else:
+        print(texto)
+
+
+if __name__ == "__main__":
+    main()
